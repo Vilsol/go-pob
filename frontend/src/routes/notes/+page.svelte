c884c640213ca5ebfb8e0e74ae9c157c1ff0d937
var4609 <varun.patni1@gmail.com>
2025-02-14 16:27:12 -0600

    feat: basic notes tab with text coloring.

--- frontend/src/routes/notes/+page.svelte
+++ frontend/src/routes/notes/+page.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
   import Tiptap from '$lib/components/Tiptap.svelte';
 
-  let value = $state('<p>Hello World! 🌍️ </p>');
+  let notesDesc = `You can use Ctrl +/- (or Ctrl+Scroll) to zoom in and out and Ctrl+0 to reset.
+This field also supports different colors. Using the caret symbol (^) followed by a Hex code or a number (0-9) will set the color.
+Below are some common color codes PoB uses:`
+
 </script>
 
-<div class="w-full h-full">
-  <Tiptap bind:value />
+<div class="notes-tab w-full h-full">
+  <div class="notes-label whitespace-pre-line">
+    <p>{notesDesc}</p>
+  </div>
+
+  <div class="editor h-full">
+    <!-- TODO: Implement copy/paste functionality with support for color codes. -->
+    <Tiptap />
+  </div>
+  
 </div>
+
+<style>
+  .notes-tab {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background: black;
+    color: white;
+    padding: 10px;
+  }
+</style>
+
